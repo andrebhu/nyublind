318e97d5cd842346b359677f8ffa2c688f647427
Andre Hu <andrebhu@gmail.com>
2023-04-28 14:31:44 -0400

    A new beginning

---
+++ README.md
@@ -0,0 +1,36 @@
+
+# pythonic-news
+A Hacker News lookalike written in Python/Django, powering [https://news.python.sc](https://news.python.sc)
+
+
+
+[![screenshot](http://cdn.sebastiansteins.com/screenshot-news-python-sc.png "Screenshot")](https://news.python.sc)
+
+
+## Setup for local development
+
+### Set up virtual environment
+```shell script
+python -m venv venv/
+source venv/bin/activate
+```
+
+### Install Dependencies
+```shell script
+pip install -r requirements.txt
+```
+
+### Migrate Database
+```shell script
+python manage.py migrate
+```
+
+### Extra setup work
+* Set ```DEBUG=True``` if necessary
+* Add ```127.0.0.1``` to ```ALLOWED_HOSTS```
+
+### Run Django Server
+```shell script
+python manage.py runserver
+```
+Now you can access the website at ```127.0.0.1:8000```.
---
+++ news/templatetags/news_extra.py
@@ -0,0 +1,69 @@
+from django import template
+from django.utils.safestring import mark_safe
+
+import mistune
+
+register = template.Library()
+
+@register.inclusion_tag('news/_item_tag.html', takes_context=True)
+def news_item(context, item, **kwargs):
+    kwargs['item'] = item
+    kwargs['request_user'] = context['user']
+    return kwargs
+
+@register.inclusion_tag('news/_link_user_tag.html')
+def link_user(user):
+    return {
+        'user': user
+    }
+
+@register.simple_tag
+def user_arrows(user, item):
+    if user == item.user:
+        return ['star']
+    else:
+        res = []
+        if item.can_be_upvoted_by(user=user):
+            res.append('up')
+        if item.can_be_downvoted_by(user=user):
+            res.append('down')
+        return res
+
+
+@register.inclusion_tag('news/_more_link_tag.html', takes_context=True)
+def more_link(context):
+    request = context.request
+    page = int(request.GET.get('p', 0))
+    query_dict = request.GET.copy()
+    query_dict['p'] = page + 1
+    _more_link=request.path_info + '?' + query_dict.urlencode()
+    return {'more_link': _more_link}
+
+
+@register.inclusion_tag('news/_item_content_tag.html')
+def item_content(**kwargs):
+    return kwargs
+
+
+@register.inclusion_tag('news/_item_control_tag.html')
+def item_control(**kwargs):
+    return kwargs
+
+
+class MarkdownRenderer(mistune.Renderer):
+
+    @classmethod
+    def setup(cls):
+        renderer = cls(escape=True, hard_wrap=True)
+        return mistune.Markdown(renderer=renderer)
+
+    def header(self, text, level, raw=None):
+        level = min(level + 2, 6)
+        return super().header(text, level, raw)
+
+
+markdown = MarkdownRenderer.setup()
+
+@register.filter
+def comment_markdown(value):
+    return mark_safe(markdown(value))
